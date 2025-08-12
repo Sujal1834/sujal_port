580c44be67ee8726c428e8b10baf7a834fbb5b8c
Sujal1834 <parmarsujal1410@gmail.com>
2025-08-12 17:33:30 +0530

    update font

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -44,7 +44,7 @@ const section1 = () => {
         
         <div className="text-center md:text-left">
         <p className="text-lg text-gray-700">Hello,</p>
-        <h1 className="text-2xl sm:text-5xl font-bold leading-tight">
+        <h1 className="text-3xl sm:text-5xl font-bold leading-tight">
             I am <span className="inline-block align-bottom text-left md:text-left sm:text-left text-bgcolor" style={{ width: '12ch' }}>{text}<span classNameName="blinking-cursor text-black font-thin ml-1">|</span> </span>
         </h1>
         <p className="mt-4 text-xl text-gray-800">MScIT Student / Tech Enthusiast</p>
