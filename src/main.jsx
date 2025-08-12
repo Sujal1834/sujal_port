a86d0d7250899e8a7d14cd4e47f70a46e93008b9
Sujal1834 <parmarsujal1410@gmail.com>
2025-08-12 16:22:29 +0530

    Updated main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import section1 from './components/section1.jsx'
 
 createRoot(document.getElementById('root')).render(
     <App />
